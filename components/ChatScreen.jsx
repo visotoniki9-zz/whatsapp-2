47a42cf7e453aa8a2e8d100f9b021975544f4c7a
visotoniki9 <d.simon.ssx0@gmail.com>
2021-05-25 00:48:17 +0200

    Added messages rendering

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -7,11 +7,17 @@ import { Avatar, IconButton } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import { useCollection } from 'react-firebase-hooks/firestore';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+import { useState } from 'react';
+import SendIcon from '@material-ui/icons/Send';
+import firebase from 'firebase';
 import { auth, db } from '../firebase';
 import Message from './Message';
 
-function ChatScreen() {
+function ChatScreen({ chat, messages }) {
   const [user] = useAuthState(auth);
+  const [input, setInput] = useState('');
   const router = useRouter(auth);
   const [messagesSnapshot] = useCollection(
     db
@@ -33,6 +39,27 @@ function ChatScreen() {
         />
       ));
     }
+    return JSON.parse(messages).map((message) => (
+      <Message key={message.id} user={message.user} message={message} />
+    ));
+  };
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    // Update last seen
+    db.collection('users').doc(user.uid).set(
+      { lastSeen: firebase.firestore.FieldValue.serverTimestamp() },
+      { merge: true },
+    );
+
+    db.collection('chats').doc(router.query.id).collection('messages').add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user.email,
+      photoURL: user.photoURL,
+    });
+
+    setInput('');
   };
 
   return (
@@ -60,6 +87,19 @@ function ChatScreen() {
         {showMessages()}
         <EndOfMessage />
       </MessageContainer>
+
+      <InputContainer>
+        <IconButton>
+          <InsertEmoticonIcon />
+        </IconButton>
+        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        <IconButton disabled={!input} type="submit" onClick={sendMessage}>
+          <SendIcon />
+        </IconButton>
+        <IconButton>
+          <MicIcon />
+        </IconButton>
+      </InputContainer>
     </Container>
   );
 }
@@ -68,6 +108,27 @@ export default ChatScreen;
 
 const Container = styled.div`
     `;
+
+const Input = styled.input`
+flex:1;
+outline:0;
+border:none;
+border-radius:10px;
+background-color:whitesmoke;
+padding:20px;
+margin-left:15px;
+margin-right:15px;
+`;
+
+const InputContainer = styled.form`
+display:flex;
+align-items:center;
+padding:10px;
+position:sticky;
+bottom:0;
+background-color:white;
+z-index:100;
+`;
 
 const Header = styled.div`
         position: sticky;
