b755200551086e51bc8e91ee39b52265e04a2927
visotoniki9 <d.simon.ssx0@gmail.com>
2021-05-25 02:23:50 +0200

    Finished chat

--- components/Message.jsx
+++ components/Message.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { useAuthState } from 'react-firebase-hooks/auth';
 import styled from 'styled-components';
+import moment from 'moment';
 import { auth } from '../firebase';
 
 function Message({ user, message }) {
@@ -11,14 +12,20 @@ function Message({ user, message }) {
 
   return (
     <Container>
-      <TypeOfMessage>{message.message}</TypeOfMessage>
+      <TypeOfMessage>
+        {message.message}
+        <Timestamp>
+          {message.timestamp ? moment(message.time).format('LT') : '...'}
+        </Timestamp>
+      </TypeOfMessage>
     </Container>
   );
 }
 
 export default Message;
 
-const Container = styled.div``;
+const Container = styled.div`
+`;
 
 const TypeOfMessage = styled.p`
 width: fit-content;
@@ -29,7 +36,7 @@ min-width:60px;
 padding-bottom:26px;
 position:relative;
 text-align:right;
-
+display:flex;
 `;
 
 const Sender = styled(TypeOfMessage)`
@@ -40,4 +47,14 @@ background-color: #dcf8c6
 const Reciever = styled(TypeOfMessage)`
 text-align: left;
 background-color: whitesmoke;
+`;
+
+const Timestamp = styled.span`
+color:gray;
+padding:10px;
+font-size: 9px;
+position:absolute;
+bottom:0;
+text-align:right;
+right:0;
 `;
